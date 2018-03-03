d7266ea79f21697d4699813de448027f2c762de7
lstearns86 <lstearns86@gmail.com>
2018-03-03 15:31:22 -0500

    Initial Commit of Stable Versions
    Moved code to GitHub

---
+++ Version1_HololensAndFingerCamera/CameraServerApp_VisualStudio/MagnificationServer/Server.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MagnificationServer
+{
+    class Server
+    {
+        public enum State { Searching, Connecting, Connected }
+        State state = State.Searching;
+        public State CurrentState { get { return state; } }
+
+        internal class ClientInfo
+        {
+            public string Name;
+            public IPAddress Address;
+            public bool Connected = false;
+            public TcpListener CommandListener = null;
+            public TcpClient CommandClient = null;
+            public NetworkStream CommandStream = null;
+            public TcpListener VideoListener = null;
+            public TcpClient VideoClient = null;
+            public NetworkStream VideoStream = null;
+        }
+
+        /// <summary>
+        /// Gets the current Wifi IP address (if connected). May return null if no connection is available
+        /// </summary>
+        IPAddress WifiAddress
+        {
+            get
+            {
+                List<string> ipAddrList = new List<string>();
+                foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+                {
+                    if (item.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 && item.OperationalStatus == OperationalStatus.Up)
+                    {
+                        foreach (UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
+                        {
+                            if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                            {
+                                return ip.Address;
+                            }
+                        }
+                    }
+                }
+                return null;
+            }
+        }
+
+        private int udpPort = 8888, commandPort = 8889, videoPort = 8899;
+
+        Dictionary<string, ClientInfo> clients = new Dictionary<string, ClientInfo>();
+
+        public delegate void ClientsUpdatedDelegate(List<ClientInfo> clients);
+        public event ClientsUpdatedDelegate ClientsUpdated;
+        private void OnClientsUpdated() { ClientsUpdated?.Invoke(clients.Values.ToList()); }
+
+        public delegate void ClientConnectedDelegate(ClientInfo client);
+        public event ClientConnectedDelegate ClientConnected;
+        private void OnClientConnected(ClientInfo client) { ClientConnected?.Invoke(client); }
+
+        public delegate void ClientDisconnectedDelegate(ClientInfo client);
+        public event ClientDisconnectedDelegate ClientDisconnected;
+        private void OnClientDisconnected(ClientInfo client) { ClientDisconnected?.Invoke(client); }
+
+        public delegate void MessageReceivedDelegate(string message);
+        public event MessageReceivedDelegate MessageReceived;
+        private void OnMessageReceived(string message) { MessageReceived?.Invoke(message); }
+
+        public delegate void ImageReceivedDelegate(Bitmap img);
+        public event ImageReceivedDelegate ImageReceived;
+        private void OnImageReceived(Bitmap img) { ImageReceived?.Invoke(img); }
+
+        public void ScanForClients()
+        {
+            Task.Run(() =>
+            {
+                var server = new UdpClient(udpPort);
+                var responseData = Encoding.ASCII.GetBytes(Environment.MachineName);
+
+                while (true)
+                {
+                    try
+                    {
+                        var clientEp = new IPEndPoint(IPAddress.Any, 0);
+                        var clientRequestData = server.Receive(ref clientEp);
+                        var clientRequest = Encoding.ASCII.GetString(clientRequestData);
+
+                        Debug.WriteLine("Detected {0} ({1}), sending response", clientRequest, clientEp.Address.ToString());
+                        server.Send(responseData, responseData.Length, clientEp);
+
+                        if (!clients.ContainsKey(clientEp.Address.ToString()))
+                            clients.Add(clientEp.Address.ToString(), new ClientInfo() { Name = clientRequest, Address = clientEp.Address });
+                        OnClientsUpdated();
+                    }
+                    catch (Exception ex) { Debug.WriteLine("Error scanning for UDP clients: " + ex.ToString()); }
+                }
+            });
+        }
+
+        TcpListener tcpListener = null;
+        public async void Connect()
+        {
+            try
+            {
+                if (tcpListener != null) { tcpListener.Stop(); }
+                Debug.WriteLine("Attempting to accept TCP connection");
+                tcpListener = new TcpListener(WifiAddress, commandPort);
+                tcpListener.Start();
+                var tempClient = await tcpListener.AcceptTcpClientAsync();
+                var tempStream = tempClient.GetStream();
+
+                var endpoint = tempClient.Client.RemoteEndPoint as IPEndPoint;
+
+                if (!clients.ContainsKey(endpoint.Address.ToString())) { Debug.WriteLine("Invalid connection"); tempStream.Close(); tempClient.Close(); return; }
+
+                var client = clients[endpoint.Address.ToString()];
+                client.CommandListener = tcpListener;
+                client.CommandClient = tempClient;
+                client.CommandStream = tempStream;
+
+                Debug.WriteLine("Connected to " + endpoint.Address.ToString() + ", receiving handshake");
+
+                try
+                {
+                    var reader = new StreamReader(client.CommandStream, Encoding.UTF8);
+                    string clientName = reader.ReadLine();
+                    if (clientName != client.Name) client.Name = clientName;
+
+                    Debug.WriteLine("Handshake successful, connected to " + clientName);
+
+                    Debug.WriteLine("Establishing video connection");
+
+                    client.VideoListener = new TcpListener(WifiAddress, videoPort);
+                    client.VideoListener.Start();
+                    client.VideoClient = await client.VideoListener.AcceptTcpClientAsync();
+                    client.VideoStream = client.VideoClient.GetStream();
+
+                    Debug.WriteLine("Video connection successful");
+
+                    // start listening to streams in
+                    _ = Task.Run(() =>
+                    {
+                        var commandReader = new StreamReader(client.CommandStream, Encoding.UTF8); ;
+                        try
+                        {
+                            while (true)
+                            {
+                                string message = commandReader.ReadLine();
+                                OnMessageReceived(message);
+                            }
+                        }
+                        catch { Disconnect(client); }
+                    });
+
+                    _ = Task.Run(async () =>
+                    {
+                        //BinaryReader videoReader = new BinaryReader(client.VideoStream);
+                        int length;
+                        try
+                        {
+                            while (true)
+                            {
+                                if (client.VideoStream.DataAvailable)
+                                {
+                                    byte[] metadataBuffer = new byte[4];
+                                    client.VideoStream.Read(metadataBuffer, 0, 4);
+                                    length = BitConverter.ToInt32(metadataBuffer, 0);
+                                    if (length > 0 && length < 10 * 1024 * 1024)
+                                    {
+                                        byte[] data = new byte[length];
+                                        int lenRead = await client.VideoStream.ReadAsync(data, 0, length);
+                                        if (lenRead == length)
+                                        {
+                                            using (var imgStream = new MemoryStream(data))
+                                            {
+                                                try
+                                                {
+                                                    Bitmap img = new Bitmap(imgStream);
+                                                    img.RotateFlip(RotateFlipType.RotateNoneFlipXY);
+                                                    OnImageReceived(img);
+                                                    img.Dispose();
+                                                }
+                                                catch { Debug.WriteLine("Error: received corrupted image"); }
+                                            }
+                                        }
+                                        data = null;
+                                        GC.GetTotalMemory(true);
+                                    }
+                                }
+                            }
+                        }
+                        catch { Disconnect(client); }
+                    });
+
+                    Thread.Sleep(3000);
+
+                    client.Connected = true;
+
+                    OnClientConnected(client);
+                }
+                catch { Debug.WriteLine("Handshake failed"); }
+            }
+            catch
+            {
+                Debug.WriteLine("Connection failed");
+            }
+        }
+
+        public void DisconnectAll()
+        {
+            foreach (string ip in clients.Keys)
+            {
+                ClientInfo client = clients[ip];
+                if (client.VideoStream != null) { client.VideoStream.Close(); client.VideoStream = null; }
+                if (client.VideoClient != null) { client.VideoClient.Close(); client.VideoClient = null; }
+                if (client.VideoListener != null) { client.VideoListener.Stop(); client.VideoListener = null; }
+                if (client.CommandStream != null) { client.CommandStream.Close(); client.CommandStream = null; }
+                if (client.CommandClient!= null) { client.CommandClient.Close(); client.CommandClient = null; }
+                if (client.CommandListener != null) { client.CommandListener.Stop(); client.CommandListener = null; }
+            }
+            clients.Clear();
+        }
+
+        public void Disconnect(ClientInfo client)
+        {
+            if (client.VideoStream != null) { client.VideoStream.Close(); client.VideoStream = null; }
+            if (client.VideoClient != null) { client.VideoClient.Close(); client.VideoClient = null; }
+            if (client.VideoListener != null) { client.VideoListener.Stop(); client.VideoListener = null; }
+            if (client.CommandStream != null) { client.CommandStream.Close(); client.CommandStream = null; }
+            if (client.CommandClient != null) { client.CommandClient.Close(); client.CommandClient = null; }
+            if (client.CommandListener != null) { client.CommandListener.Stop(); client.CommandListener = null; }
+
+            OnClientDisconnected(client);
+        }
+
+        public void SendImage(Bitmap img)
+        {
+            List<ClientInfo> disconnectedClients = new List<ClientInfo>();
+
+            try
+            {
+                // encode the image as a jpeg and convert it to a byte array
+                MemoryStream imgStream = new MemoryStream();
+                img.Save(imgStream, ImageFormat.Jpeg);
+                byte[] bytes = imgStream.ToArray();
+
+                foreach (string ip in clients.Keys)
+                {
+                    ClientInfo client = clients[ip];
+                    if (!client.Connected) continue;
+                    try
+                    {
+                        // write first the size (# of bytes) and then the image data
+                        client.VideoStream.Write(BitConverter.GetBytes((int)imgStream.Length), 0, 4);
+                        client.VideoStream.Write(bytes, 0, (int)imgStream.Length);
+                        client.VideoStream.Flush();
+                        //Debug.WriteLine("Sent image of size " + bytes.Length + " to " + client.Name);
+
+                        //if (client.VideoStream.DataAvailable)
+                        //    Debug.WriteLine("Client has data available");
+                    }
+                    catch { Disconnect(client); disconnectedClients.Add(client); }
+                }
+            }
+            catch(Exception ex) { Debug.WriteLine("Error sending image: " + ex.ToString()); }
+
+            foreach (ClientInfo client in disconnectedClients) clients.Remove(client.Address.ToString());
+        }
+
+        public void SendText(string message)
+        {
+            try
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(message + "\n");
+                foreach (string ip in clients.Keys)
+                {
+                    ClientInfo client = clients[ip];
+                    if (!client.Connected) continue;
+                    try
+                    {
+                        client.CommandStream.Write(bytes, 0, bytes.Length);
+                        Debug.WriteLine("Sent message to " + client.Name + ": " + message);
+                    }
+                    catch { Disconnect(client); }
+                }
+            }
+            catch (Exception ex) { Debug.WriteLine("Error sending text: " + ex.ToString()); }
+        }
+    }
+}
---
+++ Version1_HololensAndFingerCamera/HololensApp_Unity/Assets/HeadsUpDisplay.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using HoloLensCameraStream;
+using UnityEngine.VR.WSA.Input;
+
+public class HeadsUpDisplay : MonoBehaviour {
+
+    const int hudWidth = 1268;
+    const int hudHeight = 700;
+    const int windowSizeDefault = 300;
+    const int testMessageSize = 3000;
+    int[] defaultFontSizes = new int[] { 60, 50, 44, 36, 28, 24 };
+    int[] defaultTextPositions = new int[] { 130, 65, 5, -50, -100, -140 };
+
+    public Canvas HeadsUpDisplayCanvas;
+    public RawImage OuterMask;
+    public RawImage Thumbnail;
+    public Text DebugText;
+    public Text TestMessage;
+
+    Vector3 dragStartPosition, imageStartPosition;
+
+    private float zoom = 1;
+    private float minZoom = 1;
+    private float scale = 1;
+    private Vector2 resolution;
+    private float dragStartZoom = 1, dragStartScale = 1;
+
+    // Use this for initialization
+    void Start () {
+
+        DebugText.text = "";
+        TestMessage.gameObject.SetActive(false);
+    }
+
+    public void SetDebugText(string text)
+    {
+        DebugText.text = text;
+    }
+
+    public void StartDragging(Vector3 handPosition)
+    {
+        dragStartPosition = handPosition;
+        imageStartPosition = OuterMask.rectTransform.anchoredPosition;
+        dragStartScale = scale;
+        dragStartZoom = zoom;
+    }
+
+    public void SetResolution(int width, int height)
+    {
+        this.resolution = new Vector2(width, height);
+        Thumbnail.texture = new Texture2D(width, height, TextureFormat.BGRA32, false);
+        zoom = Math.Max((float)windowSizeDefault / width, (float)windowSizeDefault / height);
+        minZoom = zoom;
+        OuterMask.rectTransform.sizeDelta = new Vector2(windowSizeDefault * scale, windowSizeDefault * scale);
+        Thumbnail.rectTransform.sizeDelta = new Vector2(width * scale * zoom, height * scale * zoom);
+        //DebugText.text = "Zoom: " + zoom + ", Scale: " + scale + ", Width: " + width + ", Height: " + height;
+    }
+
+    private void ConstrainPosition()
+    {
+        //float x = OuterMask.rectTransform.anchoredPosition.x;
+        //float y = OuterMask.rectTransform.anchoredPosition.y;
+        //if (x < -hudWidth / 2) x = -hudWidth / 2;
+        //if (x + OuterMask.rectTransform.rect.width > hudWidth / 2) x = hudWidth / 2 - OuterMask.rectTransform.rect.width;
+        //if (y > hudHeight / 2) y = hudHeight / 2;
+        //if (y - OuterMask.rectTransform.rect.height < -hudHeight / 2) y = -hudHeight / 2 + OuterMask.rectTransform.rect.height;
+        //OuterMask.rectTransform.anchoredPosition = new Vector3(x, y);
+    }
+
+    public void ResetPosition()
+    {
+        //OuterMask.rectTransform.anchoredPosition = new Vector3(hudWidth / 2 - OuterMask.rectTransform.rect.width, hudHeight / 2);
+        OuterMask.rectTransform.anchoredPosition = new Vector2(0.5f, 0.5f);
+        //OuterMask.rectTransform.pos
+    }
+
+    public void UpdateDragging(Vector3 handPosition, MagnificationManager.DragMode dragMode)
+    {
+        Vector3 delta = handPosition - dragStartPosition;
+        delta = Camera.main.transform.InverseTransformDirection(delta);
+
+        if (dragMode == MagnificationManager.DragMode.Move)
+        {
+            float newX = imageStartPosition.x + delta.x * 3000;
+            float newY = imageStartPosition.y + delta.y * 3000;
+            SetPosition(new Vector2(newX, newY));
+        }
+        else if(dragMode == MagnificationManager.DragMode.Scale)
+        {
+            float dragFactor = 1;
+            if(Math.Abs(delta.x) > Math.Abs(delta.y)) dragFactor = delta.x; else dragFactor = delta.y;
+            dragFactor = (float)Math.Pow(2, dragFactor * 3);
+            SetScale(dragStartScale * dragFactor);
+            //DebugText.text = "Zoom: " + zoom + ", Scale: " + scale + ", Width: " + resolution.x + ", Height: " + resolution.y;
+        }
+        else if (dragMode == MagnificationManager.DragMode.Zoom)
+        {
+            float dragFactor = 1;
+            if (Math.Abs(delta.x) > Math.Abs(delta.y)) dragFactor = delta.x; else dragFactor = delta.y;
+            dragFactor = (float)Math.Pow(2, dragFactor * 3);
+            SetZoom(dragStartZoom * dragFactor);
+            //DebugText.text = "Zoom: " + zoom + ", Scale: " + scale + ", Width: " + resolution.x + ", Height: " + resolution.y;
+        }
+    }
+
+    public void SetPosition(Vector2 position, bool anchorCenter = false)
+    {
+        if(anchorCenter)
+            OuterMask.rectTransform.anchoredPosition = new Vector2(position.x - OuterMask.rectTransform.rect.width / 2, position.y + OuterMask.rectTransform.rect.height / 2);
+        else
+            OuterMask.rectTransform.anchoredPosition = position;
+        ConstrainPosition();
+    }
+
+    public Vector2 GetPosition()
+    {
+        return OuterMask.rectTransform.anchoredPosition;
+    }
+
+    public void SetDistance(float distance)
+    {
+        HeadsUpDisplayCanvas.planeDistance = distance;
+        //HeadsUpDisplayCanvas.scaleFactor = distance;
+    }
+
+    public float GetDistance()
+    {
+        return HeadsUpDisplayCanvas.planeDistance;
+    }
+
+    public void SetScale(float scale)
+    {
+        this.scale = scale;
+        OuterMask.rectTransform.sizeDelta = new Vector2(windowSizeDefault * scale, windowSizeDefault * scale);
+        Thumbnail.rectTransform.sizeDelta = new Vector2(resolution.x * scale * zoom, resolution.y * scale * zoom);
+        TestMessage.rectTransform.localScale = new Vector3(scale, scale, 1);
+        //TestMessage.rectTransform.sizeDelta = new Vector2(testMessageSize * scale, testMessageSize * scale);
+        //Text[] lines = TestMessage.GetComponentsInChildren<Text>();
+        //for(int i = 0; i < lines.Length; i++)
+        //{
+        //    Text line = lines[i];
+        //    //line.rectTransform.localScale = new Vector3(1 / scale, 1 / scale, 1);
+        //    line.fontSize = (int)Math.Round(defaultFontSizes[i] * scale);
+        //    line.rectTransform.anchoredPosition = new Vector2(0, defaultTextPositions[i] * scale);
+        //}
+        ConstrainPosition();
+    }
+
+    public float GetScale()
+    {
+        return scale;
+    }
+
+    public void SetZoom(float zoom)
+    {
+        if (zoom < minZoom) zoom = minZoom;
+        this.zoom = zoom;
+        OuterMask.rectTransform.sizeDelta = new Vector2(windowSizeDefault * scale, windowSizeDefault * scale);
+        Thumbnail.rectTransform.sizeDelta = new Vector2(resolution.x * scale * zoom, resolution.y * scale * zoom);
+    }
+
+    public float GetZoom()
+    {
+        return zoom;
+    }
+
+    public void ShowTestMessage()
+    {
+        TestMessage.gameObject.SetActive(true);
+        Thumbnail.gameObject.SetActive(false);
+    }
+
+    public void HideTestMessage()
+    {
+        TestMessage.gameObject.SetActive(false);
+        Thumbnail.gameObject.SetActive(true);
+    }
+}
